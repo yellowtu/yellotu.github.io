fdaac7ac95cf179f0f9dc47c76175d5d2605d8b3
yellotu <2194726228@qq.com>
2017-07-20 21:12:22 +0800

    add tast.html

---
+++ js/index.js
@@ -0,0 +1,12 @@
+  $("p").hide();
+  $("#quiz").show(2000);
+
+  function add(){
+    $("#answer").show(2000)
+  }
+  function app(){
+    setTimeout("add()",2000)
+  }
+  app();
+  $(".nav").hide();
+  $(".nav").show(2000);
