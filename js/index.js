0e4530793f596545df78309c634acc8d4774063a
yellotu <2194726228@qq.com>
2017-07-24 18:09:10 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -9,3 +9,15 @@
   app();
   $(".nav").hide();
   $(".nav").show(2000);
+  $(".head_portrait").mouseenter(
+    function(){
+    $(".bokeming").animate({ marginTop:'215px'});
+
+});
+  $(".head_portrait").mouseleave(
+    function(){
+    $(".bokeming").animate({ marginTop:'250px'});
+
+});
+
+
