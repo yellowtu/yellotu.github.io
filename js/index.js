b2e687d877c2e4ef3fc57074072ecb38818e7aec
yellotu <2194726228@qq.com>
2017-08-07 14:15:18 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
   $("p").hide();
   $("#quiz").show(2000);
-  function add(){
+
+
+    setTimeout(function add(){
     $("#answer").show(2000)
-  }
-  function app(){
-    setTimeout("add()",2000)
-  }
+  },2000)
+
   app();
   $(".nav").hide();
   $(".nav").show(2000);
